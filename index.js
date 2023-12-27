eb59e16b3fc3343567498d60666d4397cadef06f
gabrielkey <gabrielgoldsa@gmail.com>
2023-12-26 22:14:40 -0300

    update: version v0.2

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 let heroi = {
     nome: "Boni Souza",
-    xp: 2001,
+    xp: 1001,
     nivel: null
 };
 
-if (heroi.xp <= 1000) {
-    heroi.nivel = "Ferro";
-} else if (heroi.xp <= 2000) {
-    heroi.nivel = "Bronze";
-} else if (heroi.xp <= 5000) {
-    heroi.nivel = "Prata";
-} else if (heroi.xp <= 7000) {
-    heroi.nivel = "Ouro";
-} else if (heroi.xp <= 8000) {
-    heroi.nivel = "Platina";
-} else if (heroi.xp <= 9000) {
-    heroi.nivel = "Ascendente";
-} else if (heroi.xp <= 10000) {
-    heroi.nivel = "Imortal";
-} else {
-    heroi.nivel = "Radiante";
-}
+let resultado = (heroi.xp <= 1000) ? heroi.nivel = "Ferro" : (heroi.xp <= 2000) ? heroi.nivel = "Bronze" : (heroi.xp <= 5000) ? heroi.nivel = "Prata" : (heroi.xp <= 7000) ? heroi.nivel = "Ouro" : (heroi.xp <= 8000) ? heroi.nivel = "Platina" : (heroi.xp <= 9000) ? heroi.nivel = "Ascendente" : (heroi.xp <= 10000) ? heroi.nivel = "Imortal" : heroi.nivel = "Radiante";
 
 console.log(`O Herói de ${heroi.nome} está no ${heroi.nivel}`);
